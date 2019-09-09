d887984163eb65f10bd912783d406919789695a3
Saeed Tabrizi <saeed@nowcando.com>
2019-09-09 19:58:33 +0430

    chore:  initial commit

---
+++ test/master.test.ts
@@ -0,0 +1,22 @@
+import "jest";
+
+
+// tslint:disable:no-empty
+beforeAll(() => { });
+
+beforeEach(() => {
+
+});
+
+afterAll(() => { });
+
+afterEach(() => { });
+
+describe("Master Test",  () => {
+
+  it("first test",  () => {
+    expect.assertions(1);
+    expect(36).toEqual(36) ;
+  });
+
+});
